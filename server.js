9d170b3004b5e4f41666dc2965cf50233e648bce
Vítor Gouveia <vitorgg@gmail.com>
2021-02-11 11:54:25 +0000

    Run server.js no port 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 var path = require('path');
 
 const app = express();
-const port = 80;
+const port = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
