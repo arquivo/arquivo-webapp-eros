cac763774b5fc18748fd187904ad150faf07026e
Paulo Cabrita <pcabritafccn@gmail.com>
2021-02-17 10:44:21 +0000

    Latest developments: homepage

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { resolveInclude } = require('ejs');
 const express = require('express')
 
 var path = require('path');
@@ -9,11 +10,59 @@ const port = 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//setup public folder
+// starts website Routes ////////////////////////////////////////////////////////////////////////////////
+
+// Homepage 
 app.use(express.static('./public'));
 app.get('/',function (req, res) {
 
-  res.render('pages/home')
+  res.render('pages/home');
 });
 
+// Pages search results
+app.use(express.static('./public'));
+app.get('/pages-search-results',function (req, res) {
+
+  res.render('pages/pages-search-results');
+});
+
+// Images search results
+app.use(express.static('./public'));
+app.get('/images-search-results',function (req, res) {
+
+  res.render('pages/images-search-results');
+});
+
+// Pages: landing page
+app.use(express.static('./public'));
+app.get('/pages',function (req, res) {
+
+  res.render('pages/pages');
+});
+
+// Pages advanced search
+app.use(express.static('./public'));
+app.get('/pages-advanced-search',function (req, res) {
+
+  res.render('pages/pages-advanced-search');
+});
+
+// Images: landing page
+app.use(express.static('./public'));
+app.get('/images',function (req, res) {
+
+  res.render('pages/images');
+});
+
+// Images: advanced search
+app.use(express.static('./public'));
+app.get('/images-advanced-search',function (req, res) {
+
+  res.render('pages/images-advanced-search');
+});
+// ends website Routes ////////////////////////////////////////////////////////////////////////////////
+
+
+// Open browser port: ${port} 
+//////////////////////////////////////////////////////////////////////////////
 app.listen(port, () => console.log(`MasterEJS app Started on port ${port}!`));
