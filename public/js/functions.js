91c5ee5e65be40d1fbde4d3b660ed9641ab310b5
Paulo Cabrita <pcabritafccn@gmail.com>
2021-03-04 05:06:59 +0000

    Latest developments: homepage, page and image pages

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,34 +1,68 @@
-// JavaScript Document
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// JavaScript functions for Arquivo.pt ///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// MENU and OPTIONS button: left and right ////////////////////////////////////////
-
-// starts menu opening and closing button for top nav left menu button
+// starts: menu opening and closing button for top nav left menu button //////////////////////////////////
 function openLeftMenuNav() {
   document.getElementById("left-nav").style.cssText = "left:0px;"; 
- // document.getElementById("menu-overlay").style.cssText = "display:block;z-index:0;background: #000; opacity:0.5;";
 }
 function closeLeftMenuNav() {
   document.getElementById("left-nav").style.cssText = "left:-200px";
- // document.getElementById("menu-overlay").style.cssText = "display:none !important;z-index:-1;"; 
-// ends menu opening and closing button for top nav left menu button
 }
-// starts menu opening and closing button for top nav right menu button
+// ends: menu opening and closing button for top nav left menu button ////////////////////////////////////
+
+// starts: menu opening and closing button for top nav right menu button  ////////////////////////////////
+
 function openOptionstMenuNav() {
   document.getElementById("options-right-nav").style.cssText = "right:0px;"; 
- // document.getElementById("menu-overlay").style.cssText = "display:block;z-index:0;background: #000; opacity:0.5;";
 }
 function closeOptionsMenuNav() {
   document.getElementById("options-right-nav").style.cssText = "right:-200px";
- // document.getElementById("menu-overlay").style.cssText = "display:none !important;z-index:-1;"; 
-// ends menu opening and closing button for top nav right menu button
 }
-// starts menu opening and closing button for top nav right menu button
+// ends: menu opening and closing button for top nav right menu button ///////////////////////////////////
+
+// starts: menu opening and closing button for top nav right menu button /////////////////////////////////
 function openRightMenuNav() {
     document.getElementById("replay-right-nav").style.cssText = "right:0px;"; 
-   // document.getElementById("menu-overlay").style.cssText = "display:block;z-index:0;background: #000; opacity:0.5;";
-  }
-  function closeRightMenuNav() {
-    document.getElementById("replay-right-nav").style.cssText = "right:-200px";
-   // document.getElementById("menu-overlay").style.cssText = "display:none !important;z-index:-1;"; 
-  // ends menu opening and closing button for top nav right menu button
 }
+function closeRightMenuNav() {
+    document.getElementById("replay-right-nav").style.cssText = "right:-200px";  
+}
+// ends: menu opening and closing button for top nav right menu button 
+
+// starts: copy URL to clipboard on click 
+function copyURlToClipboard() {
+}
+// ends: copy URL to clipboard on click 
+
+// starts: alerts messages  
+
+// alert message in
+function alertsMessageIn() {
+  document.getElementById("alert-container").style.cssText = "top:0;height:47px;opacity:1;transition:0.3s"; 
+  setTimeout("document.getElementById('alert-container')", 2000); 
+}
+// alert message out
+function alertsMessageOut() {
+  document.getElementById("alert-container").style.cssText = "height:0px;opacity:0;transition:0.5s";     
+}
+// ends: alerts message  /////////////////////////////////////////////////////////////////////////////////
+
+// starts: accordion function ////////////////////////////////////////////////////////////////////////////
+
+// opens accordion
+function openAdvancedSearchForm(){
+ document.getElementById('accordion').style.display = "block";
+ document.getElementById('closeAdvancedSearch').style.display = "block";
+return false;
+}
+// closes accordion
+function closeAdvancedSearch(){
+ document.getElementById('accordion').style.display = "none";
+return false;
+}
+// ends: accordion function /////////////////////////////////////////////////////////////////////////////
+
+
+
+
