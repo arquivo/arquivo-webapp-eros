b140a63abc257509c6441dad728130f51b52a787
Vasco Rato <vasco.rato@fccn.pt>
2021-03-25 19:39:53 +0000

    implemented working page search

---
+++ search-pages.js
@@ -0,0 +1,38 @@
+const https = require('https');
+const url = require('url');
+const now = new Date();
+
+const getApiData = function(apiRequestData){
+    return new Promise((resolve,reject) => {
+        
+    });
+}
+
+module.exports = function (req,res) {
+
+    const requestData = new URL(req.url,req.protocol+'://'+req.headers.host).searchParams;
+    const apiRequestData = new URLSearchParams({
+        q: requestData.get('q') ?? '',
+        from: requestData.has('dateStart') ? requestData.get('dateStart').split('/').reverse().join('')  : '19960101',
+        to: (requestData.get('dateEnd') ?? now.toLocaleDateString('pt-PT')).split('/').reverse().join(''),
+        maxItems: 10,
+    });
+    let apiReply = ''
+    https.get('https://arquivo.pt/pagesearch/textsearch?'+apiRequestData.toString(),
+        (response) => {
+            if (response.statusCode != 200) {
+                console.log('Invalid status code <' + response.statusCode + '>');
+            } else {
+                response.on('data', (d) => {apiReply = apiReply+d.toString()});
+                response.on('end', () => {
+                    const apiData = JSON.parse(apiReply);
+                    console.log('it worked! n: '+apiData.response_items.length);
+                    res.render('pages/pages-search-results',{
+                        requestData: requestData,
+                        apiData: apiData
+                    });
+                });
+            }
+            
+        });
+}
