b1d3a544c716bf307c507696a5423561c9d6c588
Vasco Rato <vasco.rato@fccn.pt>
2021-03-30 10:49:03 +0100

    made exports work

--- public/js/init.js
+++ public/js/init.js
@@ -1,4 +1,6 @@
+// Common procedures to run on every page load
 $(function(){
+    //make sure we have a modal div
     let modal = $('#modal');
     if(!modal.length){
         $('body').append('<div id="modal"></div>');
