ff3505557e137e4dd76dcabefe025d36f658dc0e
Vasco Rato <vasco.rato@fccn.pt>
2021-05-06 11:02:41 +0100

    Added URL search

---
+++ cdx-api.js
@@ -0,0 +1,26 @@
+const https = require('https');
+const config = require('config');
+
+module.exports = function (apiRequestData, callback) {
+    let apiReply = '';
+    let apiData = [];
+    const apiRequest = https.get(config.get('cdx.api') + '?' + apiRequestData.toString(),
+        (apiRes) => {
+            apiRes.on('data', (d) => { 
+                apiReply = apiReply + d.toString();
+                let endIndex = apiReply.indexOf('}') +1;
+                while(endIndex > 0){
+                    apiData.push(JSON.parse(apiReply.slice(0,endIndex)))
+                    apiReply = apiReply.slice(endIndex+1)
+                    endIndex = apiReply.indexOf('}') +1;
+                }
+            });
+            apiRes.on('end', () => {
+
+            });
+            apiRes.on('close', () => {
+                callback(apiData);
+            });
+
+        });
+}
