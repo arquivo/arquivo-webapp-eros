a082d5916ea6e912ea0b0239b87b138a936104f7
Vasco Rato <vasco.rato@fccn.pt>
2021-05-19 18:24:27 +0100

    Implemented basic url search

--- cdx-api.js
+++ cdx-api.js
@@ -6,13 +6,13 @@ module.exports = function (apiRequestData, callback) {
     let apiData = [];
     const apiRequest = https.get(config.get('cdx.api') + '?' + apiRequestData.toString(),
         (apiRes) => {
-            apiRes.on('data', (d) => { 
+            apiRes.on('data', (d) => {
                 apiReply = apiReply + d.toString();
-                let endIndex = apiReply.indexOf('}') +1;
-                while(endIndex > 0){
-                    apiData.push(JSON.parse(apiReply.slice(0,endIndex)))
-                    apiReply = apiReply.slice(endIndex+1)
-                    endIndex = apiReply.indexOf('}') +1;
+                let endIndex = apiReply.indexOf('}') + 1;
+                while (endIndex > 0) {
+                    apiData.push(JSON.parse(apiReply.slice(0, endIndex)))
+                    apiReply = apiReply.slice(endIndex + 1)
+                    endIndex = apiReply.indexOf('}') + 1;
                 }
             });
             apiRes.on('end', () => {
