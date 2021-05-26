c43734efba72e721d01e2e6c2568c5986a9f7282
Vasco Rato <vasco.rato@fccn.pt>
2021-05-26 16:16:51 +0100

    Final implementation of CDX filter

--- src/filter-cdx.js
+++ src/filter-cdx.js
@@ -1,47 +1,48 @@
 // makes export json from Api request and reply
 module.exports = function (apiData) {
     const delta = 3600;
+    let prevKnown200Index = -1;
+    let nextKnown200Index = -1;
     const returnData = apiData
         // Sanity check
         .filter((item) => (item.status && item.timestamp && item.digest && item.url))
         // Only 200 and 300 requests are accepted
         .filter(item => (item.status[0] == '2' || item.status[0] == '3'))
-        // Sort by digest to find duplicates
-        .sort(sortByDigest)
-        // On duplicates (same digest) show only the oldest version
-        .filter((item, index, array) => {
-            return (index == 0 || item.digest != array[index - 1].digest)
-        })
-        // Sort by timestamp to properly order the results
-        .sort(sortByTimestamp)
+        // Filter out redirects to itself
         .filter((item, index, array) => {
             // all with status 200 automatically pass
             if (item.status[0] == '2') {
+                prevKnown200Index = index;
                 return true;
             }
-            // filter out if previous is 200 within delta timeframe
-            if (
-                index > 0 && // make sure there's a previous index
-                array[index - 1].status[0] == '2' && timestampDifferenceInSeconds(array[index - 1].timestamp, item.timestamp) <= delta // actual condition
-            ) {
+            // filter out there is a previous item with status 200 within delta timeframe
+            if ( prevKnown200Index >= 0 && timestampDifferenceInSeconds(array[prevKnown200Index].timestamp, item.timestamp) <= delta ) {
                 return false;
+            }
+            // find the next item with 200 status 
+            if(nextKnown200Index < index){
+                nextKnown200Index = index+1;
+                while (nextKnown200Index < array.length && array[nextKnown200Index].status[0] != '2'){
+                    nextKnown200Index++;
+                }
             }
             // filter out if next is 200 within delta timeframe
-            if (
-                index < array.length - 1 && // make sure there's a next index
-                array[index + 1].status[0] == '2' && timestampDifferenceInSeconds(item.timestamp, array[index + 1].timestamp) <= delta // actual condition
-            ) {
+            if ( nextKnown200Index < array.length && timestampDifferenceInSeconds(array[nextKnown200Index].timestamp, item.timestamp) <= delta ) {
                 return false;
             }
+            
             return true;
         })
+        // Sort by digest to find duplicates
+        .sort(sortByDigest)
+        // On status 200 duplicates (same digest) on the same day, show only the oldest version
+        .filter((item, index, array) => {
+            return (index == 0 || item.status[0] != '2' || item.digest != array[index - 1].digest || item.timestamp.substring(6, 8) != array[index - 1].timestamp.substring(6, 8))
+        })
+        // Sort by timestamp to properly order the results
+        .sort(sortByTimestamp)
         ;
 
-    console.log(returnData.length);
-    apiData.filter(x => !x.status).forEach(item => {
-        console.log(item);
-    });
-    console.log(returnData.filter(x => x.status[0] == '2').length);
     return returnData;
 }
 // Sorts items by digest. If digest is the same, sorts them by timestamp.
@@ -69,5 +70,5 @@ function getDateFromTimestamp(ts) {
 function timestampDifferenceInSeconds(ts1, ts2) {
     let tsd1 = getDateFromTimestamp(ts1);
     let tsd2 = getDateFromTimestamp(ts2);
-    return (tsd2.getTime() - tsd1.getTime()) / 1000;
+    return Math.abs((tsd2.getTime() - tsd1.getTime()) / 1000);
 }
