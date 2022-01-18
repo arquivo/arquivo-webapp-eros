64e07d435e868786aba38701de35aa06b10835fc
Vasco Rato <vasco.rato@fccn.pt>
2022-01-18 17:51:19 +0000

    fixed tracking

--- src/tracking.js
+++ src/tracking.js
@@ -1,20 +1,19 @@
-
+const config = require('config');
 module.exports = function (req, res, type) {
 
     // http://localhost:3000/image/view/trackingID/timestamp/archivedUrl
     const logger = require('./logger')(type+'Tracking');
-    const splitPath = (req.params.url + (req.params['0'] ?? '')).split('/');
-    
+    const splitPath = (req.url).split('/');
     const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const userAgent = req.get('user-agent');
     const requestUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    const trackingID = splitPath[0];
+    const trackingID = splitPath[3];
     const sessionID = req.session.id;
-    const timestamp = splitPath[1];
-    const archivedUrl = decodeURIComponent(splitPath[2]);
+    const timestamp = splitPath[4];
+    const archivedUrl = decodeURIComponent(splitPath[5]);
 
     const logString = `'${ipAddress}'\t"${userAgent}"\t'${requestUrl}'\t'${trackingID}'\t'${sessionID}'\t'${timestamp}'\t'${archivedUrl}'`;
     logger.info(logString);
 
-    res.redirect('/wayback/'+timestamp+'/'+archivedUrl);
+    res.redirect(config.get('wayback.url')+'/'+timestamp+'/'+archivedUrl);
 }
