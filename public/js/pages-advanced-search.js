aeeb4dbf412e99381fcebb7a2f79722290307517
Vasco Rato <vasco.rato@fccn.pt>
2022-08-17 20:10:56 +0100

    fixed small bugs with multi format on advanced page search [ https://github.com/arquivo/pwa-technologies/issues/1111 ]

--- public/js/pages-advanced-search.js
+++ public/js/pages-advanced-search.js
@@ -2,18 +2,17 @@ $(() => {
     const form = $('form#advanced-search-form-pages');
     const formatInput = form.find('input[name=format]').first();
     form.submit(function(e) {
-        let checkedTypes = form.find('input[type=checkbox]:checked').toArray().map(x => x.name).map(x => x.split('.').pop());
+        let checkedTypes = form.find('input[type=checkbox]:checked').toArray().map(x => $(x).attr('format'));
         if(checkedTypes.some(x => x == 'all')){
             formatInput.val('all')
         } else {
             formatInput.val(checkedTypes.join(','));
         }
-        form.find('input[type=checkbox]').attr("disabled", "disabled")
         return true;
     })
 
-    const allCheckbox = form.find('input[name="format.all"]');
-    const otherCheckboxes = form.find('input[type=checkbox]:not([name="format.all"])');
+    const allCheckbox = form.find('input[format="all"]');
+    const otherCheckboxes = form.find('input[type=checkbox]:not([format="all"])');
 
     allCheckbox.change(function(){
         otherCheckboxes.prop("checked", this.checked );
@@ -21,8 +20,12 @@ $(() => {
     otherCheckboxes.change(function(){
         if(!this.checked){
             allCheckbox.prop("checked",false);
-        }else if(otherCheckboxes.find(':checked').length == otherCheckboxes.length){
+        } else if(otherCheckboxes.toArray().filter(x => x.checked).length == otherCheckboxes.length){
             allCheckbox.prop("checked",true);
         }
+    });
+    
+    $('table#table-format label').click(function(e){
+        $(e.target).closest('td').prev().find('input').click();
     })
 });
