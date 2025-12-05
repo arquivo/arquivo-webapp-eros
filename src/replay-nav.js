2ede699879291d59e6d6c88b58fa0729435da0c4
Ivo Branco <ivo.branco@fccn.pt>
2025-12-05 16:08:32 +0000

    test: add replay-nav tests

--- src/replay-nav.js
+++ src/replay-nav.js
@@ -2,6 +2,15 @@ const sanitizeInputs = require('./utils/sanitize-search-params');
 const CDXSearchApiRequest = require('./apis/cdx-api')
 const cdxFilter = require('./filter-cdx')
 
+/**
+ * Replay navigation handler
+ * 
+ * Fetches CDX data for a given URL and renders the replay navigation partial.
+ * This is used to display the timeline/calendar navigation for archived pages.
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 module.exports = function (req, res) {
     const requestData = sanitizeInputs(req, res);
     const apiRequest = new CDXSearchApiRequest();
