f9b8d441a3262759ebe25a0cf892663eec397831
Ivo Branco <igobranco@gmail.com>
2026-07-03 12:00:59 +0100

    ci: add commitlint and semantic-release workflows

---
+++ .releaserc.js
@@ -0,0 +1,25 @@
+export default {
+  branches: ['master'],
+  plugins: [
+    ['@semantic-release/commit-analyzer', {
+      preset: 'conventionalcommits',
+      releaseRules: [
+        { breaking: true, release: 'major' },
+        { type: 'feat',   release: 'minor' },
+        { type: 'fix',    release: 'patch' },
+        { type: 'perf',   release: 'patch' },
+        { type: 'revert', release: 'patch' },
+      ],
+    }],
+    ['@semantic-release/release-notes-generator', {
+      preset: 'conventionalcommits',
+    }],
+    '@semantic-release/changelog',
+    ['@semantic-release/npm', { npmPublish: false }],
+    ['@semantic-release/git', {
+      assets: ['CHANGELOG.md', 'package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]',
+    }],
+    '@semantic-release/github',
+  ],
+};
